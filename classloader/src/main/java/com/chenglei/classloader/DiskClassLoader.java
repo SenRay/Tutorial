31119805b4ae8a2177a9359606aab20dd7bce098
chenglei <chenglei518316@163.com>
2019-05-16 00:35:08 +0800

    提交

--- classloader/src/main/java/com/chenglei/classloader/DiskClassLoader.java
+++ classloader/src/main/java/com/chenglei/classloader/DiskClassLoader.java
@@ -1,4 +1,77 @@
 package com.chenglei.classloader;
 
-public class DiskClassLoader {
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class DiskClassLoader extends ClassLoader{
+
+    private String path;
+
+    public DiskClassLoader( String path) {
+        this.path = path;
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        Class clazz = null;
+
+        byte[] data = loadClassByteCode(name);
+        if (data == null)
+            throw  new ClassNotFoundException();
+        else
+            clazz = defineClass(name,data,0,data.length);
+        return clazz;
+    }
+
+    private byte[] loadClassByteCode(String name) {
+        String fileName = getFileName(name);
+        File file = new File(path,fileName);
+
+        InputStream inputStream = null;
+        ByteArrayOutputStream byteArrayOutputStream = null;
+        try {
+            inputStream = new FileInputStream(file);
+            byteArrayOutputStream = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int length = 0;
+            while ((length = inputStream.read(buffer)) != -1){
+                byteArrayOutputStream.write(buffer,0,length);
+            }
+            return byteArrayOutputStream.toByteArray();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(inputStream != null){
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (byteArrayOutputStream != null){
+                try {
+                    byteArrayOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    private String getFileName(String name) {
+        int index = name.lastIndexOf(".");
+        if (index == -1)
+            return name + ".class";
+        else
+            return name.substring(index + 1) + ".class";
+    }
 }
--- classloader/src/main/java/com/chenglei/classloader/TestClassLoader.java
+++ classloader/src/main/java/com/chenglei/classloader/TestClassLoader.java
@@ -1,4 +1,31 @@
 package com.chenglei.classloader;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class TestClassLoader {
+    public static void main(String[] args) {
+        DiskClassLoader diskClassLoader = new DiskClassLoader("C:\\GitHub\\Tutorial\\path");
+
+        try {
+            //此处的类名一定要是完整的类名
+            Class clazz = diskClassLoader.loadClass("com.chenglei.classloader.HellClassLoader");
+            if (clazz != null){
+                Object object = clazz.newInstance();
+                System.out.println(object.getClass().getClassLoader());
+                Method method = clazz.getMethod("say",null);
+                method.invoke(object,null);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
 }
